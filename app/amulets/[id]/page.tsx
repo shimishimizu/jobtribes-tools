fcf3ec29d1268fb51d1aa43db61b279c8877823f
shimishimizu <h200l700@gmail.com>
2024-06-24 19:27:06 +0900

    詳細ページの修正

--- app/amulets/[id]/page.tsx
+++ app/amulets/[id]/page.tsx
@@ -13,12 +13,17 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { notFound } from "next/navigation";
-import Status from "./_components/Status";
+import Status from "./_components/AmuletDetailStatus";
 import { useAmulet } from "./_hooks/useAmulet";
+import AmuletDetailIcons from "./_components/AmuletDetailIcons";
+import AmuletDetailTable from "./_components/AmuletDetailTable";
 
 const AmuletDetail = ({ params }: { params: { id: string } }) => {
   const {
     amulet,
+    rarityIconImage,
+    typeIconImage,
+    categoryIconImage,
     statusIncreasePerLevelValues,
     statusIncreasePerNFTValues,
     level,
@@ -32,8 +37,15 @@ const AmuletDetail = ({ params }: { params: { id: string } }) => {
     <>
       <Header />
       {amulet ? (
-        <div className="mx-auto max-w-screen-lg px-4 pb-10 pt-28">
-          <h1 className="mb-10 text-center text-xl font-bold">{amulet.name}</h1>
+        <div className="mx-auto max-w-screen-lg px-4 pb-10 pt-24">
+          <h1 className="mb-8 text-center text-xl font-bold">{amulet.name}</h1>
+
+          <AmuletDetailIcons
+            amulet={amulet}
+            rarityIconImage={rarityIconImage}
+            typeIconImage={typeIconImage}
+            categoryIconImage={categoryIconImage}
+          />
 
           <div className="mb-8 flex items-start justify-center gap-4">
             <Image
@@ -44,10 +56,7 @@ const AmuletDetail = ({ params }: { params: { id: string } }) => {
               priority
               className="w-[50%] max-w-[200px]"
             />
-            <div>
-              <p className="mb-2">{amulet.category}</p>
-              <p dangerouslySetInnerHTML={{ __html: amulet.info }}></p>
-            </div>
+            <p dangerouslySetInnerHTML={{ __html: amulet.info }}></p>
           </div>
 
           <div className="flex flex-col gap-y-10">
@@ -61,56 +70,12 @@ const AmuletDetail = ({ params }: { params: { id: string } }) => {
               />
             )}
 
-            <div>
-              {/* <h2 className="mb-2 text-center font-medium text-slate-800">
-                【スキル】
-              </h2> */}
-              <Table className="min-w-[600px] overflow-auto">
-                <TableHeader className="whitespace-nowrap bg-slate-100">
-                  <TableRow>
-                    <TableHead className="w-[150px]">スキル名</TableHead>
-                    <TableHead>詳細</TableHead>
-                    <TableHead className="text-right">消費バジェット</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {amulet.spec.skill.map((item, index) => (
-                    <TableRow key={index}>
-                      <TableCell className="font-bold">{item.title}</TableCell>
-                      <TableCell>{item.desc}</TableCell>
-                      <TableCell className="text-center">
-                        {item.budget}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </div>
-
+            <AmuletDetailTable label="スキル" amuletSkill={amulet.spec.skill} />
             {amulet.spec.ability && (
-              <div>
-                {/* <h2 className="mb-2 text-center font-medium text-slate-800">
-                  【アビリティ】
-                </h2> */}
-                <Table className="min-w-[600px] overflow-auto">
-                  <TableHeader className="whitespace-nowrap bg-slate-100">
-                    <TableRow>
-                      <TableHead className="w-[150px]">アビリティ</TableHead>
-                      <TableHead>詳細</TableHead>
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody>
-                    {amulet.spec.ability.map((item, index) => (
-                      <TableRow key={index}>
-                        <TableCell className="font-bold">
-                          {item.title}
-                        </TableCell>
-                        <TableCell>{item.desc}</TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </div>
+              <AmuletDetailTable
+                label="アビリティ"
+                amuletAbility={amulet.spec.ability}
+              />
             )}
           </div>
         </div>
