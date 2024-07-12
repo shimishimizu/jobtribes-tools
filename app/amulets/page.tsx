b56d89a92491026a7ad4f11bc6278248b509f1c3
shimishimizu <h200l700@gmail.com>
2024-07-12 14:52:54 +0900

    Googleアナリティクス、meta dataの設定

--- app/amulets/page.tsx
+++ app/amulets/page.tsx
@@ -1,6 +1,11 @@
 import Header from "@/components/Header";
 import React from "react";
 import AmuletsTable from "./_componets/AmuletsTable";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "アミュレット一覧 | JobTribes Tools",
+};
 
 const Amulets = () => {
   return (
